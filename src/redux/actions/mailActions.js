43da5e400d33d6a084998fa42d9038f7e2155cb8
iamshiv007 <iamshiv20032003@gmail.com>
2023-10-13 11:35:30 +0530

    update: proxy remove

--- src/redux/actions/mailActions.js
+++ src/redux/actions/mailActions.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 import { newMailFailed, newMailRequest, newMailSuccess } from "../reducers/mailReducer";
 
+const url = import.meta.env.VITE_BASE_URL
+
 // 1. New Mail
 export const createMail = (formData) => async (dispatch) => {
     dispatch(newMailRequest());
 
     try {
         const { data } = await axios.post(
-            "/api/mail/new", formData
+            `${url}/api/mail/new`, formData
         );
 
         dispatch(newMailSuccess(data));
